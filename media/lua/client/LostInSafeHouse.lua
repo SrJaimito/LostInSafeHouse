20a7a458229915bcad62feded2734bf1770bc819
SrJaimito <jaime.senors@gmail.com>
2022-01-23 13:20:49 +0100

    Add check for player in safe house

--- media/lua/client/LostInSafeHouse.lua
+++ media/lua/client/LostInSafeHouse.lua
@@ -7,18 +7,25 @@ local function LISH_OnKeyPressed(keycode)
     -- Check if user pressed hyphen key
     if keycode == 53 then
         if not listIsShowing then
-            local core = getCore()
-            local screenWidth = core:getScreenWidth()
-            local screenHeight = core:getScreenHeight()
+            local player = getPlayer()
+            local playerLocation = player:getCurrentSquare()
+            local playerSafeHouse = SafeHouse.getSafeHouse(playerLocation)
 
-            local width = 400
-            local height = screenHeight / 2
-            local x = (screenWidth - width) / 2
-            local y = (screenHeight - height) / 2
+            -- Are you in your safe house?
+            if playerSafeHouse and (not SafeHouse.isSafeHouse(playerLocation, player:getUsenrame(), true)) then
+                local core = getCore()
+                local screenWidth = core:getScreenWidth()
+                local screenHeight = core:getScreenHeight()
 
-            safeHouseItemsList = SafeHouseItemsList:new(x, y, width, height);
-            safeHouseItemsList:initialise();
-            safeHouseItemsList:addToUIManager();
+                local width = 400
+                local height = screenHeight / 2
+                local x = (screenWidth - width) / 2
+                local y = (screenHeight - height) / 2
+
+                safeHouseItemsList = SafeHouseItemsList:new(x, y, width, height);
+                safeHouseItemsList:initialise();
+                safeHouseItemsList:addToUIManager();
+            end
         else
             safeHouseItemsList:setVisible(false);
             safeHouseItemsList:removeFromUIManager();
